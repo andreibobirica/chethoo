8c621924508f3815087217f41ea37cc64a439b88
AndreiMac <andreibobirica99@gmail.com>
2021-03-23 14:25:57 +0100

    Modifica del branch per nonf are confusione tra gli argomenti
    non si devono fare branch a caso da argomenti non completati, hai fatto il branch login registration da image risizer, non va bene.
    Fai sempre da master.
    Mi sono ritrovato facendo il merge cose a caso di image resizer.
    Inoltre ho spostato le robe delle view dentro /core/view/

--- dbcopier/built/index.js
+++
@@ -1,6 +0,0 @@
-import { Copier } from "./Copier.js";
-import { ResizerImage } from "./ResizerImage.js";
-let cp = new Copier();
-cp.run();
-let resizer = new ResizerImage();
-resizer.run();
--- dbcopier/src/index.ts
+++
@@ -1,9 +0,0 @@
-import {Copier} from "./Copier.js";
-import {ResizerImage} from "./ResizerImage.js";
-
-let cp : Copier = new Copier();
-cp.run();
-
-let resizer : ResizerImage = new ResizerImage();
-resizer.run();
-
